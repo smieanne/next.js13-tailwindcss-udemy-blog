5e6d42b81c8007ce083f8dee1847a9870ad271cd
smieanne <mieannen.sugiyama@g-seed.co.jp>
2024-04-17 23:16:56 +0900

    initial commit

---
+++ app/components/ArticleList.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Article } from "../types";
+import ArticleCard from "./ArticleCard";
+
+type ArticleListProps = {
+  articles: Article[];
+};
+
+const ArticleList = ({ articles }: ArticleListProps) => {
+  return (
+    <div>
+      {articles.map((article) => (
+        <ArticleCard article={article} key={article.id} />
+      ))}
+    </div>
+  );
+};
+
+export default ArticleList;
